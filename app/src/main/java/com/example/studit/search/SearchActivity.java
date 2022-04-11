ba33ee19a685c8df899224e32123c02381ab4e28
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-04-11 18:21:53 +0900

    fix: correct manifest and image view search

--- app/src/main/java/com/example/studit/search/SearchActivity.java
+++ app/src/main/java/com/example/studit/search/SearchActivity.java
@@ -1,4 +1,16 @@
 package com.example.studit.search;
 
-public class SearchActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.studit.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
 }
