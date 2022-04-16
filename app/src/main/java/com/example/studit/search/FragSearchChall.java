8da62b908225255720f59b11aa3f36e4db7bf6c0
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-04-17 00:23:42 +0900

    feat: add challenge list in the search activity

--- app/src/main/java/com/example/studit/search/FragSearchChall.java
+++ app/src/main/java/com/example/studit/search/FragSearchChall.java
@@ -7,8 +7,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.studit.R;
+
+import java.util.ArrayList;
 
 public class FragSearchChall  extends Fragment {
     private View view;
@@ -21,10 +26,27 @@ public class FragSearchChall  extends Fragment {
         return fragSearchChall;
     }
 
+    private final ArrayList<FragSearchChallengeModel> challengeModelArrayList = new ArrayList<>();
+    RecyclerView recyclerView;
+    FragSearchChallengeAdapter challengeAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_search_chall, container, false);
+
+        challengeModelArrayList.add(new FragSearchChallengeModel("도전 가능", "챌린지 같이하실 분~~!", "#아이스버킷 #챌린지", "35%"));
+        challengeModelArrayList.add(new FragSearchChallengeModel("도전 가능", "챌린지 같이하실 분~~!", "#아이스버킷 #챌린지", "55%"));
+        challengeModelArrayList.add(new FragSearchChallengeModel("도전 가능", "챌린지 같이하실 분~~!", "#아이스버킷 #챌린지", "66%"));
+
+        recyclerView = view.findViewById(R.id.search_challenge_list);
+        recyclerView.setHasFixedSize(true);
+
+        challengeAdapter = new FragSearchChallengeAdapter(challengeModelArrayList, getContext());
+        recyclerView.setAdapter(challengeAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         return view;
     }
---
+++ app/src/main/java/com/example/studit/search/FragSearchChallengeModel.java
@@ -0,0 +1,43 @@
+package com.example.studit.search;
+
+public class FragSearchChallengeModel {
+    String state;
+    String title;
+    String tag;
+    String progress;
+
+    public FragSearchChallengeModel(String state, String title, String tag, String time) {
+        this.state = state;
+        this.title = title;
+        this.tag = tag;
+        this.progress = time;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getProgress() { return progress; }
+
+    public void setProgress(String progress) { this.progress = progress; }
+}
