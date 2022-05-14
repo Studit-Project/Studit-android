874c2f8f088de0e52b2e122491717b075dadf1dd
gayoung <tlsrkdud0911@naver.com>
2022-05-15 04:51:18 +0900

    feat : add auto login checkbox

--- app/src/main/java/com/example/studit_login/LoginActivity.java
+++ app/src/main/java/com/example/studit_login/LoginActivity.java
@@ -1,9 +1,13 @@
-package com.example.studit_login;
+package com.example.studit.login;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +15,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.example.studit.join.JoinActivity;
+import com.example.studit.main.MainActivity;
+import com.example.studit.R;
+import com.example.studit.main.MainActivity;
+import com.example.studit_login.LoginRequest;
+import com.example.studit_login.MainActivity;
 
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     EditText Id, Password;
-    Button Login_Button, Signup_Button, find_pwd ;
+    Button Login_Button, find_pwd ;
+    TextView Signup_Button;
+    CheckBox checkBox;
+    SharedPreferences autoLogin;
+    SharedPreferences.Editor editor;
+    Intent intent;
+    String Logout_Code;
+
+    //기본키
+    public static String sID;
 
     @Override
     protected void onCreate(Bundle savedInstancesState) {
@@ -28,18 +47,58 @@ public class LoginActivity extends AppCompatActivity {
         Password = (EditText) findViewById(R.id.password);
 
         Login_Button = (Button) findViewById(R.id.login_button);
-        Signup_Button = (Button) findViewById(R.id.signup_button);
+        Signup_Button = (TextView) findViewById(R.id.signup_button);
 
-        //pwd 찾기
-        find_pwd = (Button) findViewById(R.id.find_pwd);
+        checkBox = (CheckBox) findViewById(R.id.autoCheck);
 
-        //회원가입 버튼 클릭(추후연동)
-        //Signup_Button.setOnClickListener(v -> {
-        //    Intent intent = new Intent(getApplicationContext(),JoinActivity.class);
+        autoLogin = getSharedPreferences("autoLogin", 0);
+        editor = autoLogin.edit();
 
-        //});
+        intent = getIntent();
+        Logout_Code = intent.getStringExtra("Logout_Code");
+        Log.d("sujung", Logout_Code);
 
-        //비밀번호 찾기 클릭
+        // 자동로그인
+        if(autoLogin.getBoolean("chk_auto", false)) {
+        // 초기 싪행시 설정 값이 없으므로 false가 반환되어 if문 작동하지 않음
+
+            // Edittext에 setText로 적용
+            Id.setText(autoLogin.getString("ID", ""));
+            Password.setText(autoLogin.getString("Password", ""));
+
+            sID = autoLogin.getString("ID", "");
+            Log.d("sujung", sID);
+
+            // 자동로그인이 활성화 되어 checkbox 활성화 표시
+            checkBox.setChecked(true);
+
+            if(Logout_Code.equals("f")){
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                // 자동 로그인 후 로그인 액티비티를 종료하여 메인 엑티비티에서 뒤로가기 버튼을 누를 경우
+                // 앱 종료
+            }
+
+            // 메인 액티비티에서 로그아웃을 할 경우
+            else {
+                Id.setText(autoLogin.getString("", ""));
+                Password.setText(autoLogin.getString("", ""));
+
+                editor.clear();
+                editor.commit();
+                return;
+            }
+        }
+
+//        //pwd 찾기(미해결)
+//        find_pwd = (Button) findViewById(R.id.find_pwd);
+
+        //회원가입 버튼 클릭
+        Signup_Button.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+            startActivity(intent);
+        });
 
         //로그인 버튼 클릭
         Login_Button.setOnClickListener(view -> {
@@ -51,19 +110,41 @@ public class LoginActivity extends AppCompatActivity {
                     JSONObject jsonResponse = new JSONObject(response);
                     boolean success = jsonResponse.getBoolean("success");
                     if (success) {
-                        Toast.makeText(getApplicationContext(), "환영합니다.", Toast.LENGTH_SHORT).show();
+                        // 자동 로그인 체크시
+                        if(checkBox.isChecked()) {
+                            Toast.makeText(getApplicationContext(), "환영합니다.", Toast.LENGTH_SHORT).show();
 
-                        String userID1 = jsonResponse.getString("userID");
-                        String userPassword1 = jsonResponse.getString("userPassword");
+                            String userID1 = jsonResponse.getString("userID");
+                            String userPassword1 = jsonResponse.getString("userPassword");
 
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
-                        // 로그인시 사용자 정보 넘기기
-                        intent.putExtra("userID", userID1);
-                        intent.putExtra("userPassword", userPassword1);
-                        startActivity(intent);
+                            editor.putString("ID", userID1);
+                            editor.putString("Password", userPassword1);
+                            editor.putBoolean("chk_auto", true);
+                            editor.commit();
 
-                    } else {
+                            // 로그인시 사용자 정보 넘기기
+                            intent.putExtra("userID", userID1);
+                            intent.putExtra("userPassword", userPassword1);
+                            startActivity(intent);
+                        }
+                        // 자동 로그인 체크 안한 경우
+                        else {
+                            editor.clear();
+                            editor.commit();
+
+                            Toast.makeText(getApplicationContext(), "환영합니다.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    //아이디 혹은 비밀번호가 입력 안된 경우
+                    if(userID.length() == 0 || userPassword.length() == 0){
+                        Toast.makeText(this, "아이디 또는 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    else {
                         Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
                         return;
                     }
