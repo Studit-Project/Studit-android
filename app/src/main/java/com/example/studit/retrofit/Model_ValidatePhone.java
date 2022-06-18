3279189820ed7692effdd69811b9e1dff1ed95cf
Hyesoo Song <shyesoo723@gmail.com>
2022-06-19 02:11:13 +0900

    add: retrofit code added

--- app/src/main/java/com/example/studit/retrofit/Model_ValidatePhone.java
+++ app/src/main/java/com/example/studit/retrofit/Model_ValidatePhone.java
@@ -6,35 +6,31 @@ public class Model_ValidatePhone {
 
     @SerializedName("phone")
     private String phone;
-    @SerializedName("numStr")
+
+    @SerializedName("response.body()")
     private String numStr;
 
-    public Model_ValidatePhone(String phone, String numStr) {
+    public Model_ValidatePhone(String phone) {
         this.phone = phone;
-        this.numStr = numStr;
     }
 
     public void setPhone(){
         this.phone = phone;
     }
 
-    public String getPhone(){
-        return phone;
-    }
+    public String getPhone(){ return phone; }
 
     public void setNumStr(){
         this.numStr = numStr;
     }
 
-    public String getNumStr(){
-        return numStr;
-    }
+    public static String getNumStr(String numStr){ return numStr; }
 
     @Override
     public java.lang.String toString() {
         return "Model_ValidatePhone{" +
                 "phone='" + phone +
-                ", numStr='" + numStr +
+                "numStr='" + numStr +
                 '}';
     }
 }
