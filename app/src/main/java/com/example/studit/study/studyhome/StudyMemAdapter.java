b5c40219cd5141d9e6700c6e7667db337940a028
gayoung <tlsrkdud0911@naver.com>
2022-08-09 23:44:21 +0900

    feat : add studymanagement

--- app/src/main/java/com/example/studit/study/studyhome/StudyMemAdapter.java
+++ app/src/main/java/com/example/studit/study/studyhome/StudyMemAdapter.java
@@ -16,9 +16,9 @@ import java.util.List;
 
 public class StudyMemAdapter extends RecyclerView.Adapter <StudyMemAdapter.ViewHolder> {
     Context context;
-    private List<StudyHomeActivity.Recycler_item> items; // 리사이클러뷰 안에 들어갈 값 저장
+    private List<StudyMemModel> items; // 리사이클러뷰 안에 들어갈 값 저장
 
-    public StudyMemAdapter(List<StudyHomeActivity.Recycler_item> items) {
+    public StudyMemAdapter(List<StudyMemModel> items) {
         this.items = items;
     }
 
@@ -32,7 +32,7 @@ public class StudyMemAdapter extends RecyclerView.Adapter <StudyMemAdapter.ViewH
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.textView.setText(items.get(position).id);
+        holder.name.setText(items.get(position).name);
     }
 
     @Override
@@ -41,14 +41,14 @@ public class StudyMemAdapter extends RecyclerView.Adapter <StudyMemAdapter.ViewH
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-        TextView textView;
+        ImageView leader;
+        TextView name;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            imageView = itemView.findViewById(R.id.my_study_gird_profile);
-            textView = itemView.findViewById(R.id.my_study_grid_name);
+            leader = itemView.findViewById(R.id.leader);
+            name = itemView.findViewById(R.id.my_study_grid_name);
         }
     }
 }
