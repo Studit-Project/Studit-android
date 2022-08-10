b2d4dd9716e0e93f087c36dc63bf6b6b9c0e1dc3
Hyesoo Song <shyesoo723@gmail.com>
2022-08-10 23:30:45 +0900

    add : join/detail model file added

--- app/src/main/java/com/example/studit/retrofit/join/Model_UserId.java
+++ app/src/main/java/com/example/studit/retrofit/join/Model_UserId.java
@@ -9,12 +9,48 @@ public class Model_UserId {
     @SerializedName("userId")
     private long userId;
 
-    public Model_UserId(long userId) {
+    @Expose
+    @SerializedName("birth")
+    private String birth;
+    @Expose
+    @SerializedName("gender")
+    private String gender;
+    @Expose
+    @SerializedName("nickname")
+    private String nickname;
+
+    public Model_UserId(int userId, String birth, String gender, String nickname) {
         this.userId = userId;
+        this.birth = birth;
+        this.gender = gender;
+        this.nickname = nickname;
     }
 
     public long getUserId() { return userId; }
 
-    public void setUserId(long userId) { this.userId= userId; }
+    public void setUserId(int userId) { this.userId= userId; }
+
+    public String getBirth() { return birth; }
+
+    public void setBirth(String birth) { this.birth = birth; }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getNickname() { return nickname; }
+
+    public void setNickname(String nickname) { this.nickname = nickname; }
+
+    @Override
+    public java.lang.String toString() {
+        return "Model_UserJoin{" +
+                "birth=" + birth + ", "+
+                "gender=" + gender + ", "+
+                "nickname=" + nickname +
+                '}';
+    }
+
+
 
 }
