444ea4f9961d8f57cad80e6aa57c69c0ff8b807d
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-08-11 23:56:01 +0900

    add : fix link and correct an error home

--- app/src/main/java/com/example/studit/retrofit/Link.java
+++ app/src/main/java/com/example/studit/retrofit/Link.java
@@ -1,8 +1,8 @@
 package com.example.studit.retrofit;
 
 public class Link {
-    public static String BASE_URL = "http://3.34.52.62:8081/";
-    public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlIjoidXNlciIsIm15TmFtZSI6InN0cmluZyIsImV4cCI6MTY2MDA3NjA4MywiaWF0IjoxNjYwMDU4MDgzfQ.eXjVg6JARJgQeIDG2fm0BFTbF3SkJS1SGUozLxEgvfU";
+    public static String BASE_URL = "http://13.209.68.199:8081/";
+    public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlIjoidXNlciIsIm15TmFtZSI6InN0cmluZyIsImV4cCI6MTY2MDI0MTcwNiwiaWF0IjoxNjYwMjIzNzA2fQ.JpaFlhFElcitGkIOs-myeE19rSLZlhj7nCqnLxGRhtE";
 
     public String getBASE_URL() {
         return BASE_URL;
