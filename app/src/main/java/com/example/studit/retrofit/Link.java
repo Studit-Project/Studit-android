ee6ee86f672c74847c1f217dcfa3f8d90acf41b3
minyoung kim <55653869+min0k0311@users.noreply.github.com>
2022-08-15 01:22:52 +0900

    add : add login token

--- app/src/main/java/com/example/studit/retrofit/Link.java
+++ app/src/main/java/com/example/studit/retrofit/Link.java
@@ -1,8 +1,15 @@
 package com.example.studit.retrofit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class Link {
     public static String BASE_URL = "http://13.125.250.60:8081/";
     public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtaW5hIiwicm9sZSI6InVzZXIiLCJteU5hbWUiOiJtaW5hIiwiZXhwIjoxNjYwNDIyMTMxLCJpYXQiOjE2NjA0MDQxMzF9.P2VDeO20aU4w5BvjxRMcLTcreMBlC1MSQdVMBQ4cjjk";
+
+//    private SharedPreferences preferences;
+//    preferences = this.requireActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+//    String token = preferences.getString("token", "");
 
     public String getBASE_URL() {
         return BASE_URL;
